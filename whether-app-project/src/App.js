e42d219bca2e1a2b6876e65c288450daec73d613
arshad <arshzmuhammed1@gmail.com>
2023-11-21 18:52:44 -0800

    first commit

---
+++ whether-app-project/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import WhetherApp from './Components/Whether App/WhetherApp';
+
+function App() {
+  return (
+    <div className="App">
+     <WhetherApp/>
+    </div>
+  );
+}
+
+export default App;
